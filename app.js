4b853fbee293f6e36a39720576caafa497895c5c
Ceci Alsina <cec.alsina@gmail.com>
2024-07-07 16:02:01 -0300

    cambios ..nada

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
 import express from 'express';
 import rutas from './router/rutas.js';
 import cookieParser from 'cookie-parser';
-import path from 'path';
 
-const app = express();
-const port = process.env.PORT || 3000;
+const app = express()
+const port = process.env.PORT || 3000
 
-// Middlewares
-app.use(express.static('public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+//middlewares
+app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cookieParser())
 
-app.use('/', rutas);
+app.use('/', rutas)
 
-// Ruta adicional para manejar 404
-app.use((req, res) => {
-    res.status(404).send('Página no encontrada');
-});
-
-app.listen(port, () => console.log("Servidor levantado"));
+app.listen(port, () => console.log("Servidor levantado"))
