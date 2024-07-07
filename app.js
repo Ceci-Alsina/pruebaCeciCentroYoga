88aad9dfc147bc78bf4eebfe148a9c5aa6d0841c
Ceci Alsina <cec.alsina@gmail.com>
2024-07-07 15:33:52 -0300

    configurando p-deploy

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 import express from 'express';
 import rutas from './router/rutas.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
-//middlewares
-app.use(express.static('public'))
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(cookieParser())
+// Middlewares
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.use('/', rutas)
+app.use('/', rutas);
 
-app.listen(port, () => console.log("Servidor levantado"))
+// Ruta adicional para manejar 404
+app.use((req, res) => {
+    res.status(404).send('Página no encontrada');
+});
+
+app.listen(port, () => console.log("Servidor levantado"));
--- router/rutas.js
+++ router/rutas.js
@@ -1,78 +1,85 @@
-import express from 'express'
+import express from 'express';
+import jwt, { decode } from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+import 'dotenv/config';
+import path from 'path';
 
-import jwt, { decode } from 'jsonwebtoken'
-import bcrypt from 'bcryptjs'
-import 'dotenv/config'
+const secretkey = process.env.SECRET_KEY;
 
-const secretkey = process.env.SECRET_KEY
+import {
+    obtenerGenerosGET,
+    obtenerRangosEtariosGET
+} from '../controllers/commons.js';
 
-import {obtenerGenerosGET,
-        obtenerRangosEtariosGET} from '../controllers/commons.js'
-
-import {altaContactoPOST,
+import {
+    altaContactoPOST,
     obtenerMensajesGET,
     eliminarMensajeDELETE,
-    actualizarMensajePUT} from '../controllers/mensajes.js'
+    actualizarMensajePUT
+} from '../controllers/mensajes.js';
 
-import {getUsuario} from '../controllers/usuarios.js'
+import { getUsuario } from '../controllers/usuarios.js';
 
-import {getAllProducts,
+import {
+    getAllProducts,
     getProductById,
     deleteProduct,
     createProduct,
     updateProduct,
-    getCategorias} from '../controllers/productos.js'
+    getCategorias
+} from '../controllers/productos.js';
 
-import {upload} from '../config/multerConfig.js'
+import { upload } from '../config/multerConfig.js';
 
 const verificarToken = (req, res, next) => {
-    const token = req.cookies.token
+    const token = req.cookies.token;
 
-    if(!token){
-        req.app.set("origen", req.url)
-        return res.status(403).redirect('/templates/login.html')
+    if (!token) {
+        req.app.set("origen", req.url);
+        return res.status(403).redirect('/templates/login.html');
     }
 
     try {
-        const decodificado = jwt.verify(token, secretkey)
-        req.user = decodificado
-        next()
-    } catch(error){
-        res.status(401).json('Token inválido')
+        const decodificado = jwt.verify(token, secretkey);
+        req.user = decodificado;
+        next();
+    } catch (error) {
+        res.status(401).json('Token inválido');
     }
-}
+};
 
 const existeUsuarioRecibido = async (usuario) => {
-    const usuarioEnBase = await getUsuario(usuario.username)
-    
-    if(usuarioEnBase){
-        const aux = bcrypt.compareSync(usuario.password, usuarioEnBase.PASSWORD)
-        return aux
+    const usuarioEnBase = await getUsuario(usuario.username);
+
+    if (usuarioEnBase) {
+        const aux = bcrypt.compareSync(usuario.password, usuarioEnBase.PASSWORD);
+        return aux;
     }
-    
+
     return false;
-}
+};
 
 const irAAdminMensajesGET = async (req, res) => {
-    return res.status(200).redirect('/templates/adminMensajes.html')
-}
+    return res.status(200).redirect('/templates/adminMensajes.html');
+};
 
 const irAProductosGET = async (req, res) => {
-    return res.status(200).redirect('/templates/productos.html')
-}
+    return res.status(200).redirect('/templates/productos.html');
+};
 
 const obtenerPassEncriptada = async (req, res) => {
-    const hash = bcrypt.hashSync(req.body.pass, bcrypt.genSaltSync(10))
-    return res.json(hash)
-}
-const rutas = express.Router()
+    const hash = bcrypt.hashSync(req.body.pass, bcrypt.genSaltSync(10));
+    return res.json(hash);
+};
+
+const rutas = express.Router();
 
 rutas.post('/login', async (req, res) => {
-    const origen = req.app.get("origen")
-    const user = req.body
-    
-    if(await existeUsuarioRecibido(user)){
-        const token = jwt.sign({user}, secretkey)
+    const origen = req.app.get("origen");
+    const user = req.body;
+
+    if (await existeUsuarioRecibido(user)) {
+        const token = jwt.sign({ user }, secretkey);
 
         res.cookie(
             'token',
@@ -82,49 +89,48 @@ rutas.post('/login', async (req, res) => {
                 secure: false,
                 expires: new Date(Date.now() + 3600000)
             }
-        )
+        );
         return (origen == "/adminMensajes") ?
-                irAAdminMensajesGET(req, res)
-                : irAProductosGET(req, res)
+            irAAdminMensajesGET(req, res) :
+            irAProductosGET(req, res);
     } else {
-        res.status(401).json('Credenciales no válidas')
+        res.status(401).json('Credenciales no válidas');
     }
-})
-
-rutas.get('/logout', (req, res) => {
-    res.clearCookie('token').redirect('/')
-})
-
-rutas.get('/generos', obtenerGenerosGET)
-
-rutas.get('/rangosEtarios', obtenerRangosEtariosGET)
-
-rutas.post('/altaContacto', altaContactoPOST)
-
-rutas.get('/adminMensajes', verificarToken, irAAdminMensajesGET)
-
-rutas.get('/mensajes', verificarToken, obtenerMensajesGET)
-
-rutas.delete('/mensajes', verificarToken, eliminarMensajeDELETE)
-
-rutas.put('/mensajes', verificarToken, actualizarMensajePUT)
-
-rutas.get('/productos', getAllProducts)
-rutas.get('/productos/:id', getProductById)
-rutas.delete('/productos/:id', deleteProduct)
-rutas.post('/productos', upload, createProduct); // Subida de un solo archivo
-rutas.put('/productos/:id', upload, updateProduct); // Subida de un solo archivo
-rutas.get('/categorias', getCategorias)
-
-rutas.get('/adminProductos', verificarToken, irAProductosGET)
-
-rutas.get('/passEncriptada', obtenerPassEncriptada)
-
-// Agregando ruta para la raíz
-rutas.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
+rutas.get('/logout', (req, res) => {
+    res.clearCookie('token').redirect('/');
+});
 
+rutas.get('/generos', obtenerGenerosGET);
 
-export default rutas
+rutas.get('/rangosEtarios', obtenerRangosEtariosGET);
+
+rutas.post('/altaContacto', altaContactoPOST);
+
+rutas.get('/adminMensajes', verificarToken, irAAdminMensajesGET);
+
+rutas.get('/mensajes', verificarToken, obtenerMensajesGET);
+
+rutas.delete('/mensajes', verificarToken, eliminarMensajeDELETE);
+
+rutas.put('/mensajes', verificarToken, actualizarMensajePUT);
+
+rutas.get('/productos', getAllProducts);
+rutas.get('/productos/:id', getProductById);
+rutas.delete('/productos/:id', deleteProduct);
+rutas.post('/productos', upload, createProduct); // Subida de un solo archivo
+rutas.put('/productos/:id', upload, updateProduct); // Subida de un solo archivo
+rutas.get('/categorias', getCategorias);
+
+rutas.get('/adminProductos', verificarToken, irAProductosGET);
+
+rutas.get('/passEncriptada', obtenerPassEncriptada);
+
+// Ruta para servir index.html
+rutas.get('/', (req, res) => {
+    res.sendFile(path.join(process.cwd(), 'public', 'index.html'));
+});
+
+export default rutas;
+
